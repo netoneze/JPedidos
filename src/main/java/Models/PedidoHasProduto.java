1b618dc173a80afb5f86e7b80e557729f3ab8d69
NELSON ANTONIO NETO TONEZE <netoneze@hotmail.com>
2021-04-03 09:58:02 -0300

    Adicionado model de pedidohasproduto

---
+++ src/main/java/Models/PedidoHasProduto.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models;
+
+/**
+ *
+ * @author Nelson Toneze e Yohan Pessanha
+ */
+public class PedidoHasProduto {
+    private int pedido_idPedido;
+    private int produto_id;
+    
+    public PedidoHasProduto(){
+    }
+    
+    public PedidoHasProduto(int idPedido, int produtoId){
+        this.pedido_idPedido = idPedido;
+        this.produto_id = produtoId;
+    }
+    
+    @Override
+    public String toString() {
+        String resposta = this.getPedidoId() + "\n";
+        resposta += this.getProdutoId() + "\n";
+        return resposta;
+    }
+
+    public int getPedidoId() {
+        return pedido_idPedido;
+    }
+
+    public int getProdutoId() {
+        return produto_id;
+    }
+
+    public void setPedidoId(int pedidoId) {
+        this.pedido_idPedido = pedidoId;
+    }
+
+    public void setProdutoId(int produtoId) {
+        this.produto_id = produtoId;
+    }
+    
+}

827fbbb3bdc01c67ee9df489bd3349129ddb4401
NELSON ANTONIO NETO TONEZE <netoneze@hotmail.com>
2021-04-07 20:22:03 -0300

    Implementado funcionalidade de calular total e deixar botões visiveis

--- src/main/java/Views/Funcionario/RegistrarPedido.java
+++ src/main/java/Views/Funcionario/RegistrarPedido.java
@@ -30,7 +30,6 @@ import javax.swing.table.DefaultTableModel;
  * @author yohan
  */
 public class RegistrarPedido extends javax.swing.JFrame {
-    ArrayList<JTextField> fieldsArr = new ArrayList<JTextField>();
     ProdutoDAO productController = new ProdutoDAO();
     PedidoDAO orderController = new PedidoDAO();
     ArrayList<Produto> productsList = productController.listaProdutos();
@@ -44,23 +43,18 @@ public class RegistrarPedido extends javax.swing.JFrame {
     public RegistrarPedido(String usuarioId) {
         this.usuarioId = usuarioId;
         initComponents();
+        ItensUI(true, true, true, false, false);
         insereProdutoCombox(productsList);
         setLocationRelativeTo(null);
         this.model = (DefaultTableModel) tbl_pedidos.getModel();
     }
 
-    public float calculaPrecoTotal() {
-        float total = 0;
-
-        DefaultTableModel model = (DefaultTableModel) tbl_pedidos.getModel();
-        int quantidadeLinhas = model.getRowCount();
-        
-        for (int i = 0; quantidadeLinhas > i; i++) {
-            int colValor = (int) model.getValueAt(i, 2);
-            total =+ colValor;
+    public void calculaPrecoTotal() {
+        float colValor = 0;
+        for (int i = 0; i < linha; i++) {
+            colValor += Float.valueOf(String.valueOf(tbl_pedidos.getValueAt(i, 2)));
         }
-       
-        return total;
+        label_total.setText(String.valueOf(colValor));
     }
     
     public void insereProdutoCombox(ArrayList<Produto> productsList) {
@@ -79,6 +73,22 @@ public class RegistrarPedido extends javax.swing.JFrame {
         model.setValueAt(tipo, linha, 0);
         model.setValueAt(quantidade, linha, 1);
         model.setValueAt(valor, linha, 2);
+        this.model = (DefaultTableModel) tbl_pedidos.getModel();
+    }
+    
+    public void adicionaNomeTelefoneNaLabel(){
+        String nome = field_nomecliente.getText();
+        String telefone = field_telefonecliente.getText();
+        label_nome.setText(nome);
+        label_telefone.setText(telefone);
+    }
+    
+    public void ItensUI(boolean A, boolean B, boolean C, boolean D, boolean E){
+        btn_addnometelefone.setEnabled(A); 
+        field_nomecliente.setEditable(B);
+        field_telefonecliente.setEditable(C);
+        btn_calculatotal.setEnabled(D);
+        btn_salvar.setEnabled(E);
     }
     
     /**
@@ -100,12 +110,12 @@ public class RegistrarPedido extends javax.swing.JFrame {
         jLabel5 = new javax.swing.JLabel();
         jComboBoxTipoProduto = new javax.swing.JComboBox<>();
         jSpinnerQuantidade = new javax.swing.JSpinner();
-        jButton2 = new javax.swing.JButton();
+        btn_addnometelefone = new javax.swing.JButton();
         jLabel12 = new javax.swing.JLabel();
         label_preco = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         btn_voltar = new javax.swing.JButton();
-        jButton1 = new javax.swing.JButton();
+        btn_salvar = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         tbl_pedidos = new javax.swing.JTable();
         jLabel6 = new javax.swing.JLabel();
@@ -115,6 +125,8 @@ public class RegistrarPedido extends javax.swing.JFrame {
         label_nome = new javax.swing.JLabel();
         label_telefone = new javax.swing.JLabel();
         btn_adicionar = new javax.swing.JButton();
+        label_total = new javax.swing.JLabel();
+        btn_calculatotal = new javax.swing.JButton();
 
         jRadioButton1.setText("jRadioButton1");
 
@@ -140,10 +152,10 @@ public class RegistrarPedido extends javax.swing.JFrame {
             }
         });
 
-        jButton2.setText("Adicionar Nome/Telefone");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        btn_addnometelefone.setText("Adicionar Nome/Telefone");
+        btn_addnometelefone.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                btn_addnometelefoneActionPerformed(evt);
             }
         });
 
@@ -161,7 +173,7 @@ public class RegistrarPedido extends javax.swing.JFrame {
                 .addGroup(JpanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(JpanelLayout.createSequentialGroup()
                         .addGap(104, 104, 104)
-                        .addComponent(jButton2)
+                        .addComponent(btn_addnometelefone)
                         .addGap(0, 94, Short.MAX_VALUE))
                     .addGroup(JpanelLayout.createSequentialGroup()
                         .addContainerGap()
@@ -200,7 +212,7 @@ public class RegistrarPedido extends javax.swing.JFrame {
                 .addGap(10, 10, 10)
                 .addComponent(field_telefonecliente, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton2)
+                .addComponent(btn_addnometelefone)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 35, Short.MAX_VALUE)
                 .addGroup(JpanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel12)
@@ -227,11 +239,11 @@ public class RegistrarPedido extends javax.swing.JFrame {
             }
         });
 
-        jButton1.setBackground(new java.awt.Color(51, 153, 255));
-        jButton1.setText("Salvar");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        btn_salvar.setBackground(new java.awt.Color(51, 153, 255));
+        btn_salvar.setText("Salvar");
+        btn_salvar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                btn_salvarActionPerformed(evt);
             }
         });
 
@@ -259,18 +271,43 @@ public class RegistrarPedido extends javax.swing.JFrame {
             new String [] {
                 "Tipo", "Quantidade", "Valor"
             }
-        ));
-        jScrollPane1.setViewportView(tbl_pedidos);
+        ) {
+            Class[] types = new Class [] {
+                java.lang.Object.class, java.lang.Object.class, java.lang.Float.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false, false, false
+            };
 
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jScrollPane1.setViewportView(tbl_pedidos);
+        if (tbl_pedidos.getColumnModel().getColumnCount() > 0) {
+            tbl_pedidos.getColumnModel().getColumn(0).setResizable(false);
+            tbl_pedidos.getColumnModel().getColumn(1).setResizable(false);
+            tbl_pedidos.getColumnModel().getColumn(2).setResizable(false);
+        }
+
+        jLabel6.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         jLabel6.setText("Nome:");
 
+        jLabel7.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         jLabel7.setText("Telefone:");
 
-        jLabel8.setText("Total: 119");
+        jLabel8.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        jLabel8.setText("Total:");
 
+        label_nome.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         label_nome.setText("Fulano");
         label_nome.setToolTipText("");
 
+        label_telefone.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         label_telefone.setText("666");
 
         btn_adicionar.setText("Adicionar");
@@ -280,48 +317,64 @@ public class RegistrarPedido extends javax.swing.JFrame {
             }
         });
 
+        label_total.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        label_total.setText("666");
+
+        btn_calculatotal.setText("Calcular Total");
+        btn_calculatotal.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_calculatotalActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(btn_voltar, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(262, 262, 262)
-                        .addComponent(jLabel2))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(400, 400, 400)
-                        .addComponent(jLabel6)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(label_nome)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jLabel9)
-                        .addGap(118, 118, 118)
-                        .addComponent(jLabel7)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(label_telefone)))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-            .addGroup(layout.createSequentialGroup()
-                .addGap(48, 48, 48)
-                .addComponent(Jpanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 101, Short.MAX_VALUE)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 410, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(46, 46, 46))
-            .addGroup(layout.createSequentialGroup()
                 .addGap(188, 188, 188)
                 .addComponent(btn_adicionar)
                 .addGap(249, 249, 249)
                 .addComponent(jLabel8)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(label_total, javax.swing.GroupLayout.PREFERRED_SIZE, 204, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(69, 69, 69))
+                .addComponent(btn_calculatotal)
+                .addGap(11, 11, 11)
+                .addComponent(btn_salvar)
+                .addContainerGap())
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(btn_voltar, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(262, 262, 262)
+                                .addComponent(jLabel2)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 420, Short.MAX_VALUE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(400, 400, 400)
+                                .addComponent(jLabel6)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(label_nome, javax.swing.GroupLayout.PREFERRED_SIZE, 162, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jLabel9)
+                                .addGap(18, 18, 18)
+                                .addComponent(jLabel7)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(label_telefone, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                        .addGap(48, 48, 48)
+                        .addComponent(Jpanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 101, Short.MAX_VALUE)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 410, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(46, 46, 46))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(14, 14, 14)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
@@ -340,10 +393,12 @@ public class RegistrarPedido extends javax.swing.JFrame {
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 319, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel8)
                     .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(btn_adicionar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                        .addComponent(jLabel8)
+                        .addComponent(label_total)
+                        .addComponent(btn_salvar, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(btn_calculatotal, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(btn_adicionar))
                 .addContainerGap())
         );
 
@@ -357,13 +412,14 @@ public class RegistrarPedido extends javax.swing.JFrame {
         this.dispose();
     }//GEN-LAST:event_btn_voltarActionPerformed
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void btn_salvarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_salvarActionPerformed
         
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_btn_salvarActionPerformed
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton2ActionPerformed
+    private void btn_addnometelefoneActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_addnometelefoneActionPerformed
+        adicionaNomeTelefoneNaLabel();
+        ItensUI(false, false, false, false, false);
+    }//GEN-LAST:event_btn_addnometelefoneActionPerformed
 
     private void jComboBoxTipoProdutoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBoxTipoProdutoActionPerformed
         label_preco.setText(String.valueOf(productsList.get(jComboBoxTipoProduto.getSelectedIndex()).getProdutoPreco()));
@@ -372,7 +428,13 @@ public class RegistrarPedido extends javax.swing.JFrame {
     private void btn_adicionarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_adicionarActionPerformed
         this.adicionaNaTabela("update");
         linha++;
+        ItensUI(false, false, false, true, false);
     }//GEN-LAST:event_btn_adicionarActionPerformed
+
+    private void btn_calculatotalActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_calculatotalActionPerformed
+        calculaPrecoTotal();
+        ItensUI(false, false, false, true, true);
+    }//GEN-LAST:event_btn_calculatotalActionPerformed
 
     /**
      * @param args the command line arguments
@@ -414,12 +476,13 @@ public class RegistrarPedido extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel Jpanel;
+    private javax.swing.JButton btn_addnometelefone;
     private javax.swing.JButton btn_adicionar;
+    private javax.swing.JButton btn_calculatotal;
+    private javax.swing.JButton btn_salvar;
     private javax.swing.JButton btn_voltar;
     private javax.swing.JTextField field_nomecliente;
     private javax.swing.JTextField field_telefonecliente;
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
     private javax.swing.JComboBox<String> jComboBoxTipoProduto;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel12;
@@ -437,6 +500,7 @@ public class RegistrarPedido extends javax.swing.JFrame {
     private javax.swing.JLabel label_nome;
     private javax.swing.JLabel label_preco;
     private javax.swing.JLabel label_telefone;
+    private javax.swing.JLabel label_total;
     private javax.swing.JTable tbl_pedidos;
     // End of variables declaration//GEN-END:variables
 }

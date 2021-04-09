c7e32bc7e87863031cc2ebb9604012027ba09079
NELSON ANTONIO NETO TONEZE <netoneze@hotmail.com>
2021-04-09 13:14:17 -0300

    MR

--- src/test/java/Controllers/TestePedido.java
+++ src/test/java/Controllers/TestePedido.java
@@ -9,16 +9,16 @@ import Models.*;
 public class TestePedido {
 	@Test
 	public void criarVazio() {
-		PedidoDAO daoPedido = new PedidoDAO();
-		Pedido pedido = new Pedido();
-		ArrayList<Produto> lista = new ArrayList<>();
+            PedidoDAO daoPedido = new PedidoDAO();
+            Pedido pedido = new Pedido();
+            ArrayList<Produto> lista = new ArrayList<>();
 
-		pedido.setPedidoNomeCliente("Claudio");
-		pedido.setPedidoTelefoneCliente("(11)22222-3333");
+            pedido.setPedidoNomeCliente("Claudio");
+            pedido.setPedidoTelefoneCliente("(11)22222-3333");
 		pedido.setPedidoId(1);
-		pedido.setPedidoTotal(35.50f);
-
-		daoPedido.criaPedido(pedido, lista);
+            pedido.setPedidoTotal(35.50f);
+                
+            daoPedido.criaPedido(pedido, lista);
 		assertEquals(null, daoPedido.listaPedidos());
 	}
 }

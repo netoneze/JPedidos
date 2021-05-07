9887538bae4e390de2cd13e913b41f41e7daa932
NELSON ANTONIO NETO TONEZE <netoneze@hotmail.com>
2021-05-07 20:12:37 -0300

    Adicionado casos de teste para PedidoController

--- src/test/java/ControllersTest/PedidoControllerTest.java
+++ src/test/java/ControllersTest/PedidoControllerTest.java
@@ -22,4 +22,40 @@ public class PedidoControllerTest {
                 
 		assertTrue(pedido.criaPedido(pedido, lista));
 	}
+        
+        @Test
+        public void criarPedido() throws ParseException{
+                Produto produto = new Produto();
+                Pedido pedido = new Pedido();
+                ArrayList<Produto> listaProdutos = produto.listaProdutos();
+		ArrayList<Produto> lista = new ArrayList<Produto>();
+
+                listaProdutos.get(1).setProdutoQuantidade(1);
+                listaProdutos.get(2).setProdutoQuantidade(1);
+                listaProdutos.get(3).setProdutoQuantidade(1);
+                lista.add(listaProdutos.get(1));
+                lista.add(listaProdutos.get(2));
+                lista.add(listaProdutos.get(3));
+                
+		pedido.setPedidoNomeCliente("Claudio");
+		pedido.setPedidoTelefoneCliente("(11)22222-3333");
+		pedido.setPedidoId(1);
+		pedido.setPedidoTotal(35.50f);
+                pedido.setUsuarioId(1);
+                
+		assertTrue(pedido.criaPedido(pedido, lista));
+        }
+        
+        @Test
+        public void FechaPedido() throws ParseException{
+                Pedido pedido = new Pedido();
+                                     
+		pedido.setPedidoNomeCliente("Claudio");
+		pedido.setPedidoTelefoneCliente("(11)22222-3333");
+		pedido.setPedidoId(1);
+                pedido.setPedidoStatus("fechado");
+                
+		assertTrue(pedido.alterarPedido(pedido));
+        }
+        
 }
